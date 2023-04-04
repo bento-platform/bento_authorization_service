fa0fab42dc038f8c1c8ba4d4e9ea46340f1b91d4
David Lougheed <david.lougheed@gmail.com>
2023-04-04 14:27:27 -0400

    add config file

---
+++ bento_authorization_service/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+
+from .constants import SERVICE_GROUP, SERVICE_ARTIFACT
+
+__all__ = [
+    "Config",
+]
+
+
+class Config(BaseSettings):
+    service_id: str = f"{SERVICE_GROUP}:{SERVICE_ARTIFACT}"
+    service_name: str = "Bento Reference Service"
+    service_url_base_path: str = "http://127.0.0.1:5000"  # Base path to construct URIs from
+
+    # /service-info customization
+    service_contact_url: str = "mailto:info@c3g.ca"
