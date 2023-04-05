bf8a3dde80b761e3728a06de553a13235a4b4af7
David Lougheed <david.lougheed@gmail.com>
2023-04-05 11:32:34 -0400

    db work: connection from pool, schema execution, get grant, better stubs

--- bento_authorization_service/db.py
+++ bento_authorization_service/db.py
@@ -1,6 +1,9 @@
+import aiofiles
 import asyncpg
+import contextlib
 
-from typing import Optional
+from pathlib import Path
+from typing import AsyncGenerator, Optional
 
 from .config import config
 
@@ -9,34 +12,67 @@ __all__ = [
 ]
 
 
+SCHEMA_PATH = Path(__file__).parent / "schema.sql"
+
+
+class DatabaseError(Exception):
+    pass
+
+
 class Database:
     def __init__(self, db_uri: str):
         self._db_uri = db_uri
         self._pool: Optional[asyncpg.Pool] = None
 
     async def initialize(self):
+        # Initialize the connection pool
         self._pool = await asyncpg.create_pool(self._db_uri)
+
+        # Connect to the database and execute the schema script
+        conn: asyncpg.Connection
+        async with aiofiles.open(SCHEMA_PATH, "r") as sf, self.connect() as conn:
+            async with conn.transaction():
+                await conn.execute(await sf.read())
 
     async def close(self):
         if self._pool:
             await self._pool.close()
 
-    # TODO: Context manager for getting connections from the pool, if it's set
+    @contextlib.asynccontextmanager
+    async def connect(self) -> AsyncGenerator[asyncpg.Connection, None]:
+        if self._pool is None:
+            raise DatabaseError("Pool is not available")
 
-    async def get_grant(self, id_: int):
-        pass  # TODO
+        conn: asyncpg.Connection
+        async with self._pool.acquire() as conn:
+            yield conn
+
+    async def get_grant(self, id_: int) -> Optional[dict]:  # TODO: TypedDict
+        conn: asyncpg.Connection
+        async with self.connect() as conn:
+            row: Optional[asyncpg.Record] = await conn.fetchrow(
+                "SELECT id, subject, resource, negated, permission, extra WHERE id = $1", id_)
+            return dict(row) if row else None
 
     async def get_grants(self):
-        pass  # TODO
+        conn: asyncpg.Connection
+        async with self.connect() as conn:
+            pass  # TODO
 
     async def add_grant(self, subject: dict, resource: dict, negated: bool, permission: str, extra: dict):
-        pass  # TODO
+        conn: asyncpg.Connection
+        async with self.connect() as conn:
+            pass  # TODO
 
     async def get_group(self, id_: int):
-        pass  # TODO
+        conn: asyncpg.Connection
+        async with self.connect() as conn:
+            pass  # TODO
 
     async def set_group(self, id_: int, membership: dict):
-        pass  # TODO
+        conn: asyncpg.Connection
+        async with self.connect() as conn:
+            pass  # TODO
 
 
 db = Database(config.database_uri)
