74fb3d0b4f1e01dd0889f05a369b50400f7ee914
David Lougheed <david.lougheed@gmail.com>
2023-04-05 10:49:42 -0400

    some initial structure for policy engine

---
+++ bento_authorization_service/policy_engine/evaluation.py
@@ -0,0 +1,11 @@
+# Policy evaluation
+# Given:
+#  - a token
+#  - a resource
+#  - a list of grants which may or may not apply to a token/resource
+#  - the required permissions to access the resource
+# Calculate:
+#  - whether the token has the required permissions to access the resource
+# Yield:
+#  - a boolean response
+#  - a log of the decision made, who the decision was made for (sub/client ID), when, on what, and *why
---
+++ bento_authorization_service/policy_engine/permissions.py
@@ -0,0 +1,27 @@
+# query_boolean:project
+# query_boolean:dataset
+
+# query_counts:project
+# query_counts:dataset
+
+# Data-level: interacting with data inside of data services... and triggering workflows
+# query:data - query at full access
+# download:data - download CSVs, associated DRS objects
+# delete:data - clear data from a specific data type
+#   - workflow-relevant items: (types of workflows....)
+# ingest:data
+# analyze:data
+# export:data
+
+# Project-level: only applies with 'all'
+# create:project
+# edit:project
+# delete:project
+
+# Dataset-level: only applies with projects/above
+# create:dataset
+# edit:dataset
+# delete:dataset
+
+# Permissions - meta
+# edit:permissions - can edit permissions for the resource which granted this permission only.
