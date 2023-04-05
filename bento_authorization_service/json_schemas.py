0b9f887437532172bb1eadc1e2cdccfb1e50f76b
David Lougheed <david.lougheed@gmail.com>
2023-04-05 15:13:16 -0400

    add group schemas  & reuse iss/sub + iss/client subschemas

--- bento_authorization_service/json_schemas.py
+++ bento_authorization_service/json_schemas.py
@@ -7,6 +7,32 @@ def _make_schema_id(name: str) -> str:
     return f"{config.service_url_base_path.rstrip('/')}/schemas/{name}.json"
 
 
+SUBJECT_ISSUER_AND_CLIENT_ID = {
+    "$id": _make_schema_id("subject_iss_client"),
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "SubjectIssuerAndClientID",
+    "type": "object",
+    "properties": {
+        "iss": {"type": "string"},  # Issuer
+        "client": {"type": "string"},  # Client ID - for service accounts, i.e., API tokens where sub not present
+    },
+    "required": ["iss", "client"],
+}
+
+
+SUBJECT_ISSUER_AND_SUBJECT_ID = {
+    "$id": _make_schema_id("subject_iss_sub"),
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "SubjectIssuerAndSubjectID",
+    "type": "object",
+    "properties": {
+        "iss": {"type": "string"},  # Issuer
+        "sub": {"type": "string"},  # Subject
+    },
+    "required": ["iss", "sub"],
+}
+
+
 SUBJECT_SCHEMA = {
     "$id": _make_schema_id("subject"),
     "$schema": "https://json-schema.org/draft/2020-12/schema",
@@ -25,20 +51,8 @@ SUBJECT_SCHEMA = {
             },
             "required": ["group"],
         },
-        {
-            "properties": {
-                "iss": {"type": "string"},  # Issuer
-                "client": {"type": "string"},  # Client ID - for service accounts, i.e., API tokens
-            },
-            "required": ["iss", "client"],
-        },
-        {
-            "properties": {
-                "iss": {"type": "string"},  # Issuer
-                "sub": {"type": "string"},  # Subject
-            },
-            "required": ["iss", "sub"],
-        },
+        SUBJECT_ISSUER_AND_CLIENT_ID,
+        SUBJECT_ISSUER_AND_SUBJECT_ID
     ],
     "additionalProperties": False,
 }
@@ -75,3 +89,49 @@ RESOURCE_SCHEMA = {
     "additionalProperties": False,
 }
 RESOURCE_SCHEMA_VALIDATOR = jsonschema.Draft202012Validator(RESOURCE_SCHEMA)
+
+
+GROUP_MEMBERSHIP_SCHEMA = {
+    "$id": _make_schema_id("group_membership"),
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "GroupMembership",
+    "type": "object",
+    "oneOf": [
+        {
+            "properties": {
+                "members": {
+                    "type": "array",
+                    "items": {
+                        "oneOf": [SUBJECT_ISSUER_AND_CLIENT_ID, SUBJECT_ISSUER_AND_SUBJECT_ID]
+                    },
+                },
+            },
+            "required": ["members"],
+        },
+        {
+            "properties": {
+                "expr": {
+                    "type": "array",  # bento_lib search-formatted query expression for group membership
+                },
+            },
+            "required": ["expr"],
+        },
+    ],
+    "additionalProperties": False,
+}
+GROUP_MEMBERSHIP_SCHEMA_VALIDATOR = jsonschema.Draft202012Validator(GROUP_MEMBERSHIP_SCHEMA)
+
+
+GROUP_SCHEMA = {
+    "$id": _make_schema_id("group"),
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "Group",
+    "type": "object",
+    "properties": {
+        "id": {"type": "integer"},
+        "membership": GROUP_MEMBERSHIP_SCHEMA,
+    },
+    "required": ["membership"],
+    "additionalProperties": False,
+}
+GROUP_SCHEMA_VALIDATOR = jsonschema.Draft202012Validator(GROUP_SCHEMA)
--- bento_authorization_service/routers/schemas.py
+++ bento_authorization_service/routers/schemas.py
@@ -1,17 +1,37 @@
 from fastapi import APIRouter
 
-from ..json_schemas import SUBJECT_SCHEMA, RESOURCE_SCHEMA
+from bento_authorization_service import json_schemas as js
 
 __all__ = ["schema_router"]
 
 schema_router = APIRouter(prefix="/schemas")
 
 
+@schema_router.get("/subject_iss_client.json")
+def subject_iss_client_json_schema():
+    return js.SUBJECT_ISSUER_AND_CLIENT_ID
+
+
+@schema_router.get("/subject_iss_sub.json")
+def subject_iss_sub_json_schema():
+    return js.SUBJECT_ISSUER_AND_SUBJECT_ID
+
+
 @schema_router.get("/subject.json")
 def subject_json_schema():
-    return SUBJECT_SCHEMA
+    return js.SUBJECT_SCHEMA
 
 
 @schema_router.get("/resource.json")
 def resource_json_schema():
-    return RESOURCE_SCHEMA
+    return js.RESOURCE_SCHEMA
+
+
+@schema_router.get("/group_membership.json")
+def group_membership_json_schema():
+    return js.GROUP_MEMBERSHIP_SCHEMA
+
+
+@schema_router.get("/group.json")
+def group_json_schema():
+    return js.GROUP_SCHEMA
