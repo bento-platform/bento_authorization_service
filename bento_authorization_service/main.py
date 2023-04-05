ada2ad76f98f666f3b8446674f0193c35650677c
David Lougheed <david.lougheed@gmail.com>
2023-04-05 15:35:07 -0400

    feat: full service info response

--- bento_authorization_service/main.py
+++ bento_authorization_service/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+from bento_authorization_service import __version__
+
+from .config import config
+from .constants import BENTO_SERVICE_KIND, SERVICE_TYPE
 from .db import db
 from .routers.grants import grants_router
 from .routers.groups import groups_router
@@ -25,4 +29,21 @@ async def shutdown():
 
 @app.get("/service-info")
 async def service_info():
-    return {}  # TODO
+    return {
+        "id": config.service_id,
+        "name": config.service_name,  # TODO: Should be globally unique?
+        "type": SERVICE_TYPE,
+        "description": "Authorization & permissions service for the Bento platform.",
+        "organization": {
+            "name": "C3G",
+            "url": "https://www.computationalgenomics.ca"
+        },
+        "contactUrl": config.service_contact_url,
+        "version": __version__,
+        "environment": "prod",
+        "bento": {
+            "serviceKind": BENTO_SERVICE_KIND,
+            "dataService": False,
+            "gitRepository": "https://github.com/bento-platform/bento_authorization_service",
+        },
+    }  # TODO: Git info
