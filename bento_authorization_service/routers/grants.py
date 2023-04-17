e41221c6bfae6083904793d6b8e561212e9e5977
David Lougheed <david.lougheed@gmail.com>
2023-04-17 10:29:50 -0400

    add grant endpoint stubs

--- bento_authorization_service/routers/grants.py
+++ bento_authorization_service/routers/grants.py
@@ -12,3 +12,21 @@ grants_router = APIRouter(prefix="/grants")
 @grants_router.get("/")
 async def list_grants():
     pass
+
+
+@grants_router.post("/")
+async def create_grant():
+    pass
+
+
+@grants_router.get("/{grant_id}")
+async def get_grant(grant_id: int):
+    pass
+
+
+@grants_router.delete("/{grant_id}")
+async def delete_grant(grant_id: int):
+    pass
+
+
+# EXPLICITLY: No grant updating; they are immutable.
