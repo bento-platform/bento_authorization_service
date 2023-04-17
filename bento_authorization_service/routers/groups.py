ef5c38aa99b64a8a447ab6b6e544111a3de8a2b7
David Lougheed <david.lougheed@gmail.com>
2023-04-17 10:30:00 -0400

    add group endpoint stubs

--- bento_authorization_service/routers/groups.py
+++ bento_authorization_service/routers/groups.py
@@ -10,3 +10,19 @@ groups_router = APIRouter(prefix="/groups")
 @groups_router.get("/")
 async def list_groups():
     pass
+
+
+@groups_router.get("/{group_id}")
+async def get_group(group_id: int):
+    pass
+
+
+@groups_router.delete("/{group_id}")
+async def delete_group(group_id: int):
+    # TODO: in a single transaction, delete grants which refer to the group and then delete the group.
+    pass
+
+
+@groups_router.put("/{group_id}")
+async def update_group(group_id: int):
+    pass
