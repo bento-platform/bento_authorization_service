4d980d03da67014d8d38c24c76d3c65217b0759b
David Lougheed <david.lougheed@gmail.com>
2023-04-19 14:34:07 -0400

    fix: pydantic models

--- bento_authorization_service/models.py
+++ bento_authorization_service/models.py
@@ -1,10 +1,39 @@
-from pydantic import create_model_from_typeddict
-from .types import Resource, Grant
+from pydantic import BaseModel
+from typing import Literal, Optional
 
 __all__ = [
+    "SubjectModel",
     "ResourceModel",
     "GrantModel",
 ]
 
-ResourceModel = create_model_from_typeddict(Resource)
-GrantModel = create_model_from_typeddict(Grant)
+
+class SubjectModel(BaseModel):
+    # janky way of representing the true Union type for Subject
+    # TODO: try create_model_from_typeddict in pydantic 2
+
+    everyone: Optional[Literal[True]] = None
+    group: Optional[int] = None
+    iss: Optional[str] = None
+    client: Optional[str] = None
+    sub: Optional[str] = None
+
+
+class ResourceModel(BaseModel):
+    # janky way of representing the true Union type for Resource
+    # TODO: try create_model_from_typeddict in pydantic 2
+
+    everything: Optional[Literal[True]] = None
+    project: Optional[str] = None
+    dataset: Optional[str] = None
+    data_type: Optional[str] = None
+
+
+class GrantModel(BaseModel):
+    # janky way of representing the true Union type for Grant
+    # TODO: try create_model_from_typeddict in pydantic 2
+
+    subject: SubjectModel
+    resource: ResourceModel
+    permission: str
+    extra: dict
