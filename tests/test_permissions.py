9b3c1d4997243fa1ad33dfc69b9033125e10a952
David Lougheed <david.lougheed@gmail.com>
2023-04-19 14:41:26 -0400

    test: permissions objects

---
+++ tests/test_permissions.py
@@ -0,0 +1,26 @@
+import pytest
+from bento_authorization_service.policy_engine.permissions import (
+    Permission,
+    PermissionDefinitionError,
+    QUERY_VERB,
+    DATA,
+    P_QUERY_DATA,
+    P_DELETE_DATA,
+)
+
+
+def test_recreate_permission_error():
+    with pytest.raises(PermissionDefinitionError):
+        Permission(QUERY_VERB, DATA)  # already exists
+
+
+def test_equality():
+    assert P_QUERY_DATA == P_QUERY_DATA
+    assert P_QUERY_DATA == "query:data"
+    assert P_QUERY_DATA != P_DELETE_DATA
+    assert P_QUERY_DATA != "a"
+    assert P_QUERY_DATA != 5
+
+
+def test_hash():
+    assert len({P_QUERY_DATA, P_QUERY_DATA, P_DELETE_DATA}) == 2
