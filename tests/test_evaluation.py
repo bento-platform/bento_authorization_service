ff571aad358da78f57de577d1690fe1a2ef1dfb9
David Lougheed <david.lougheed@gmail.com>
2023-04-20 12:31:11 -0400

    test: test invalid group memberships and more subject-matching

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,5 +1,8 @@
 import pytest
 from bento_authorization_service.policy_engine.evaluation import (
+    InvalidGrant,
+    InvalidGroupMembership,
+    InvalidResourceRequest,
     check_if_token_is_in_group,
     check_if_grant_subject_matches_token,
     check_if_grant_resource_matches_requested_resource,
@@ -14,6 +17,16 @@ SUB = "david"
 
 TEST_TOKEN = {
     "iss": ISS,
+    "sub": SUB,
+    "aud": "account",
+    "azp": CLIENT,
+    "typ": "Bearer",
+    "exp": 100,  # Not checked here
+    "iat": 0,  # Not checked here
+}
+
+TEST_TOKEN_FOREIGN_ISS = {
+    "iss": "https://google.com",
     "sub": SUB,
     "aud": "account",
     "azp": CLIENT,
@@ -56,6 +69,7 @@ TEST_GROUPS: list[tuple[Group, bool]] = [
     ({"id": i, "membership": x}, r)
     for i, (x, r) in enumerate(TEST_GROUP_MEMBERSHIPS)
 ]
+TEST_GROUPS_DICT: dict[int, Group] = {x["id"]: x for x, _ in TEST_GROUPS}
 
 
 TEST_GRANT_EVERYONE_EVERYTHING_QUERY_DATA: Grant = {
@@ -70,17 +84,54 @@ TEST_GRANT_EVERYONE_PROJECT_1_QUERY_DATA: Grant = {
     "permission": P_QUERY_DATA,
     "extra": {},
 }
+TEST_GRANT_GROUP_0_PROJECT_1_QUERY_DATA: Grant = {
+    "subject": {"group": 0},
+    "resource": RESOURCE_PROJECT_1,
+    "permission": P_QUERY_DATA,
+    "extra": {},
+}
+TEST_GRANT_GROUP_2_PROJECT_1_QUERY_DATA: Grant = {
+    "subject": {"group": 2},
+    "resource": RESOURCE_PROJECT_1,
+    "permission": P_QUERY_DATA,
+    "extra": {},
+}
+
+
+def test_invalid_group_membership():
+    with pytest.raises(InvalidGroupMembership):
+        check_if_token_is_in_group(TEST_TOKEN, {"id": 1000, "membership": {}})
+    with pytest.raises(InvalidGroupMembership):  # Must not be a malformatted member
+        check_if_token_is_in_group(TEST_TOKEN, {"id": 1000, "membership": {"members": [{"bad": True}]}})
+    with pytest.raises(InvalidGroupMembership):  # Must specify client or subject
+        check_if_token_is_in_group(TEST_TOKEN, {"id": 1000, "membership": {"members": [{"iss": ISS}]}})
 
 
 @pytest.mark.parametrize("group, is_member", TEST_GROUPS)
 def test_group_membership(group: Group, is_member: bool):
     assert not check_if_token_is_in_group(None, group)
     assert check_if_token_is_in_group(TEST_TOKEN, group) == is_member
+    assert not check_if_token_is_in_group(TEST_TOKEN_FOREIGN_ISS, group)  # All groups have local issuer
 
 
 def test_subject_match():
-    assert check_if_grant_subject_matches_token({}, TEST_TOKEN, TEST_GRANT_EVERYONE_EVERYTHING_QUERY_DATA)  # Everyone
-    assert check_if_grant_subject_matches_token({}, TEST_TOKEN, TEST_GRANT_EVERYONE_PROJECT_1_QUERY_DATA)  # Everyone
+    assert check_if_grant_subject_matches_token(
+        TEST_GROUPS_DICT, TEST_TOKEN, TEST_GRANT_EVERYONE_EVERYTHING_QUERY_DATA)  # Everyone
+    # Everyone (even foreign issuer):
+    assert check_if_grant_subject_matches_token(
+        TEST_GROUPS_DICT, TEST_TOKEN_FOREIGN_ISS, TEST_GRANT_EVERYONE_EVERYTHING_QUERY_DATA)
+    # No token:
+    assert check_if_grant_subject_matches_token(
+        TEST_GROUPS_DICT, None, TEST_GRANT_EVERYONE_EVERYTHING_QUERY_DATA)
+
+    assert check_if_grant_subject_matches_token(
+        TEST_GROUPS_DICT, TEST_TOKEN, TEST_GRANT_EVERYONE_PROJECT_1_QUERY_DATA)  # Everyone
+
+    assert check_if_grant_subject_matches_token(
+        TEST_GROUPS_DICT, TEST_TOKEN, TEST_GRANT_GROUP_0_PROJECT_1_QUERY_DATA)  # Member of group 0
+
+    assert not check_if_grant_subject_matches_token(
+        TEST_GROUPS_DICT, TEST_TOKEN, TEST_GRANT_GROUP_2_PROJECT_1_QUERY_DATA)  # NOT a member of group 2
 
 
 def test_resource_match():
