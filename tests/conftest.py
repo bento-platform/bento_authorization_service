93f9b50058f66a291f27eeb14a3b9dab89f9d1aa
David Lougheed <david.lougheed@gmail.com>
2023-04-21 10:40:13 -0400

    test: set up database fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
 import pytest
+import pytest_asyncio
 from fastapi.testclient import TestClient
 
-
-import sys
-print(sys.version_info)
-
-
+from bento_authorization_service.db import db as db_instance
 from bento_authorization_service.main import app
 
 
 @pytest.fixture
 def test_client():
     yield TestClient(app)
+
+
+@pytest_asyncio.fixture
+async def db():
+    await db_instance.initialize()
+    yield db_instance
