f53bc564cce154dde5fb699aa928a9d62cf4eb42
David Lougheed <david.lougheed@gmail.com>
2023-04-24 10:04:26 -0400

    test: group fetch, list, and delete endpoints

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -44,11 +44,60 @@ async def test_db_group_non_existant(db: Database):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("group, _is_member", TEST_GROUPS)
-async def test_group_creation_endpoint(group: Group, _is_member: bool, test_client: TestClient, db: Database):
+async def test_group_endpoints_creation(group: Group, _is_member: bool, test_client: TestClient, db: Database):
+    # Group can be created via endpoint
     g = {**group}
     del g["id"]
     res = test_client.post("/groups/", json=g)
     assert res.status_code == status.HTTP_201_CREATED
 
+    # Verify group exists in database
     g_rest = res.json()
     assert json.dumps(await db.get_group(g_rest["id"]), sort_keys=True) == json.dumps(g_rest, sort_keys=True)
+
+
+def test_group_endpoints_fetch_404(test_client: TestClient):
+    res = test_client.get("/groups/0")  # Below serial 1
+    assert res.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("group, _is_member", TEST_GROUPS)
+async def test_group_endpoints_fetch(group: Group, _is_member: bool, test_client: TestClient, db: Database):
+    # Create group in database directly
+    g_db = await db.create_group(group)
+
+    # Test that we can fetch the group via endpoint
+    res = test_client.get(f"/groups/{g_db}")
+    assert res.status_code == status.HTTP_200_OK
+    res_data = res.json()
+    assert json.dumps({**group, "id": g_db}, sort_keys=True) == json.dumps(res_data, sort_keys=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("group, _is_member", TEST_GROUPS)
+async def test_group_endpoints_list(group: Group, _is_member: bool, test_client: TestClient, db: Database):
+    # Create group in database directly
+    g_db = await db.create_group(group)
+
+    # Test that we can find the group via list endpoint
+    res = test_client.get(f"/groups/")
+    assert res.status_code == status.HTTP_200_OK
+    res_data = res.json()
+    group_in_list = next((g for g in res_data if g["id"] == g_db), None)
+    assert json.dumps({**group, "id": g_db}, sort_keys=True) == json.dumps(group_in_list, sort_keys=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("group, _is_member", TEST_GROUPS)
+async def test_group_endpoints_delete(group: Group, _is_member: bool, test_client: TestClient, db: Database):
+    # Create group in database directly
+    g_db = await db.create_group(group)
+
+    # Test that we can delete the group
+    res = test_client.delete(f"/groups/{g_db}")
+    assert res.status_code == status.HTTP_204_NO_CONTENT
+
+    # Test that we can delete again - it is a 404, but not an internal error
+    res = test_client.delete(f"/groups/{g_db}")
+    assert res.status_code == status.HTTP_404_NOT_FOUND
