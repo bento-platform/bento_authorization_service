f549bfa2af1133814bfa2b44e4a8bf6389c6b392
David Lougheed <david.lougheed@gmail.com>
2023-04-24 14:56:39 -0400

    test: grant endpoints

--- tests/test_grants.py
+++ tests/test_grants.py
@@ -47,3 +47,49 @@ async def test_grant_endpoints_create(db: Database, test_client: TestClient):
     if db_grant:
         db_grant["permission"] = str(db_grant["permission"])
     assert json.dumps(res_data, sort_keys=True) == json.dumps(db_grant, sort_keys=True)
+
+
+@pytest.mark.asyncio
+async def test_grant_endpoints_get(db: Database, test_client: TestClient):
+    # 404
+    res = test_client.get("/grants/0")
+    assert res.status_code == status.HTTP_404_NOT_FOUND
+
+    # create grant in database
+    g_id = await db.create_grant(sd.TEST_GRANT_DAVID_PROJECT_1_QUERY_DATA)
+    db_grant: dict = await db.get_grant(g_id)
+    if db_grant:
+        db_grant["permission"] = str(db_grant["permission"])
+
+    # test that we can fetch it
+    res = test_client.get(f"/grants/{g_id}")
+    assert res.status_code == status.HTTP_200_OK
+    assert json.dumps(res.json(), sort_keys=True) == json.dumps(db_grant, sort_keys=True)
+
+
+@pytest.mark.asyncio
+async def test_grant_endpoints_list(db: Database, test_client: TestClient):
+    # create grant in database
+    g_id = await db.create_grant(sd.TEST_GRANT_DAVID_PROJECT_1_QUERY_DATA)
+    db_grant: dict = await db.get_grant(g_id)
+    if db_grant:
+        db_grant["permission"] = str(db_grant["permission"])
+
+    # test that we can find it in the list
+    res = test_client.get(f"/grants/")
+    assert res.status_code == status.HTTP_200_OK
+    assert any(True for g in res.json() if json.dumps(g, sort_keys=True) == json.dumps(db_grant, sort_keys=True))
+
+
+@pytest.mark.asyncio
+async def test_grant_endpoints_delete(db: Database, test_client: TestClient):
+    # create grant in database
+    g_id = await db.create_grant(sd.TEST_GRANT_DAVID_PROJECT_1_QUERY_DATA)
+
+    # test that we can delete it
+    res = test_client.delete(f"/grants/{g_id}")
+    assert res.status_code == status.HTTP_204_NO_CONTENT
+
+    # test that we can delete it again (but get 404)
+    res = test_client.delete(f"/grants/{g_id}")
+    assert res.status_code == status.HTTP_404_NOT_FOUND
