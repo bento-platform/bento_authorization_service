279dca1e821a16897cc826921a541d246918e0ef
David Lougheed <david.lougheed@gmail.com>
2024-08-14 13:47:06 -0400

    refact: use bento lib function to convert log level str

--- bento_authorization_service/logger.py
+++ bento_authorization_service/logger.py
@@ -1,21 +1,15 @@
 import logging
 
+from bento_lib.logging import log_level_from_str
 from .config import get_config
 
 __all__ = [
     "logger",
 ]
 
-log_config_to_log_level = {
-    "debug": logging.DEBUG,
-    "info": logging.INFO,
-    "warning": logging.WARNING,
-    "error": logging.ERROR,
-}
-
 logging.basicConfig(level=logging.DEBUG)
 
 # TODO: convert to injectable thing for FastAPI
 
 logger = logging.getLogger(__name__)
-logger.setLevel(log_config_to_log_level[get_config().log_level])
+logger.setLevel(log_level_from_str(get_config().log_level))
